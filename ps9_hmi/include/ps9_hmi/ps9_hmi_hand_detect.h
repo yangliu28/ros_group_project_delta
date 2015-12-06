f9450e23348c6ca2c069ab77756e35f4be7a30cf
xpharry <xpharry123@hotmail.com>
2015-12-06 13:26:10 -0500

    commit of a test file, and fixed library ready for testing

--- ps9_hmi/include/ps9_hmi/ps9_hmi_hand_detect.h
+++ ps9_hmi/include/ps9_hmi/ps9_hmi_hand_detect.h
@@ -2,12 +2,11 @@
 //part of ps9_hmi package
 //written by Matthew Dobrowsky
 
-#ifndef PS9_HMI_LIB_H_
-#define PS9_HMI_LIB_H_
+#ifndef PS9_HMI_HAND_DETECT_H_
+#define PS9_HMI_HAND_DETECT_H_
 
 #include <ros/ros.h>
 #include <cwru_pcl_utils/cwru_pcl_utils.h>
-#include <geometry_msgs/Pose.h>
 #include <Eigen/Eigen> 
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
@@ -31,20 +30,26 @@ public:
 	//constructor
 	HumanMachineInterface(ros::NodeHandle* nh);
 
-    ~HumanMachineInterface(void) {  }
+   	 ~HumanMachineInterface(void) {  }
 	
 	//function for returning hand detection state
 	bool get_human_hand();
+
+	//getter methods for private values
+	double get_hand_height() { return hand_height_; };
+	double get_blocked_ratio() { return blocked_ratio_; };
 
 	CwruPclUtils cwru_pcl_utils;
 
 private:
 	//Kinect usage
 	void get_kinect_snapshot_();
+	void convert_rgb_to_xyz_(PointCloud<pcl::PointXYZRGB> inputCloud, PointCloud<pcl::PointXYZ> outputCloud);
 
     //measured hand values
 	bool hand_present_;
 	double hand_height_;
+	double blocked_ratio_;
 	Eigen::Vector3f hand_centroid_;
 
     //values for hand detection criteria
