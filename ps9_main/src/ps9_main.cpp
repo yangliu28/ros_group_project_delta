1ef15a6f34834f9ed4115656900ec22dee83f892
yangliu28 <yxl1450@case.edu>
2015-12-05 21:39:12 -0500

    add strategy of iteration on blocks

--- ps9_main/src/ps9_main.cpp
+++ ps9_main/src/ps9_main.cpp
@@ -39,6 +39,7 @@ int main(int argc, char** argv) {
     // instantiate an arm motion object
     ArmMotionCommander arm_motion_commander(&nh);
     // instantiate an human hand detection object
+    HumanMachineInterface human_machine_interface;
 
 
 
@@ -109,6 +110,8 @@ int main(int argc, char** argv) {
     rtn_val=arm_motion_commander.rt_arm_execute_planned_path();  // execute the planned path
 
     // for hand detection in human machine interface
+    bool b_human_hand_present;  // bool value to indicate current human hand status
+    bool b_continue_blocks_operation;  // bool value to indicate whether continue operation on blocks
 
 
 
@@ -116,53 +119,92 @@ int main(int argc, char** argv) {
     // THE LOOP
 
     while (ros::ok()) {
-        if (block_detection.b_get_colored_block()) {  // check if there is blocks on the stool
-            // if here, means there is new block been placed on the stool
+        // baxter should be able to recognize the following human hand signals in sequence
+            // human hand signal 1: present
+            // human hand signal 2: not present
+        // these signals indicate a permit of performing block detection and baxter movements
 
-            // get block color from point cloud
-            block_color = block_detection.get_block_color();
-
-            // get block position and orientation, and move arm to it
-            arm_position = block_detection.get_block_position();
-            arm_orientation = block_detection.get_block_orientation();
-            right_arm_pose = arm_motion_commander.set_destination_pose(arm_position, arm_orientation);
-            arm_motion_commander.move_to_destination();
-
-            // grasp the block
-            baxter_gripper_control.close_hand();
-
-            // move right arm away to a pre-defined pose
-            arm_motion_commander.move_to_pre_define_pose();
-
-            // assign different arm destination according to block color
-            // colored-block classification precedure
-            switch block_color:
-                case block_detection.get_color_blue():
-                    // if the color is blue defined in the point color class
-                    // then the arm poses is ...
-                    right_arm_pose...
-                case block_detection.get_color_red();
-                    // if the color is red defined in the point color class
-                    // then the arm poses is ...
-                    right_arm_pose...
-                case block_detection.get_color_green();
-                    // if the color is green defined in the point color class
-                    // then the arm poses is ...
-                    right_arm_pose...
-
-            // move right arm to destiantion pose
-            arm_motion_commander.set_destination_pose(right_arm_pose);
-            arm_motion_commander.move_to_destination();
-
-            // unrelease the block
-            baxter_gripper_control.open_hand();
-
-            // move right arm to pre-define pose
-            arm_motion_commander.move_to_pre_define_pose();
-
-            ros::Duration(3.0).sleep();  // delay for a while, for cleaning the stool
+        b_human_hand_present = human_machine_interface.get_human_hand();
+        while (!b_human_hand_present) {
+            // wait for 0.5 second and re-check
+            ros::Duration(0.5).sleep();
+            b_human_hand_present = human_machine_interface.get_human_hand();
+            ROS_INFO("waiting for human hand signal 1");
         }
-        ros::Duration(0.5).sleep();  // sleep for half a second
+        // if here, get an human hand presence signal
+        ROS_INFO("human hand signal 1 detected");
+
+        int time_count = 0;  // time count for hand presence
+        while (b_human_hand_present && time_count<10) {
+            ros::Duration(1.0).sleep();
+            b_human_hand_present = human_machine_interface.get_human_hand();
+            time_count = time_count + 1;  // usually an human interaction is within 10 seconds
+            // this will avoid program accidently waits here forever
+        }
+
+        if (!b_human_hand_present) {  // the while-loop exits because hand signal is detected
+            b_continue_blocks_operation = true;
+            ROS_INFO("human hand signal 2 detected");
+        }
+        else {  // the while-loop exits because time is out
+            b_continue_blocks_operation = false;
+            ROS_ERROR("time out on human hand signal 2");
+        }
+
+        // continue operation on blocks
+        if (b_continue_blocks_operation) {
+            ros::Duration(1.0).sleep();  // let people walk away
+
+            if (block_detection.b_get_colored_block()) {  // check if there is blocks on the stool
+                // if here, means there is new block been placed on the stool
+
+                // get block color from point cloud
+                block_color = block_detection.get_block_color();
+
+                // get block position and orientation, and move arm to it
+                arm_position = block_detection.get_block_position();
+                arm_orientation = block_detection.get_block_orientation();
+                right_arm_pose = arm_motion_commander.set_destination_pose(arm_position, arm_orientation);
+                arm_motion_commander.move_to_destination();
+
+                // grasp the block
+                baxter_gripper_control.close_hand();
+
+                // move right arm away to a pre-defined pose
+                arm_motion_commander.move_to_pre_define_pose();
+
+                // assign different arm destination according to block color
+                // colored-block classification precedure
+                switch block_color:
+                    case block_detection.get_color_blue():
+                        // if the color is blue defined in the point color class
+                        // then the arm poses is ...
+                        right_arm_pose...
+                    case block_detection.get_color_red();
+                        // if the color is red defined in the point color class
+                        // then the arm poses is ...
+                        right_arm_pose...
+                    case block_detection.get_color_green();
+                        // if the color is green defined in the point color class
+                        // then the arm poses is ...
+                        right_arm_pose...
+
+                // move right arm to destiantion pose
+                arm_motion_commander.set_destination_pose(right_arm_pose);
+                arm_motion_commander.move_to_destination();
+
+                // unrelease the block
+                baxter_gripper_control.open_hand();
+
+                // move right arm to pre-define pose
+                arm_motion_commander.move_to_pre_define_pose();
+
+                ros::Duration(3.0).sleep();  // delay for a while, for cleaning the stool
+            }
+            else
+                ROS_ERROR("fail to get block info");
+        }
+
         ros::spinOnce();  // let variables to update, if there is any
     }
 }
